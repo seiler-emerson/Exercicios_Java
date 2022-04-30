34bd1855c6bde30aa3e203bc57c2c7ec8de4ca6c
Emerson <seiler.emerson@gmail.com>
2022-04-30 16:42:37 -0300

    Update readme
    update readme

--- exercicio 001 - Hello World/src/br/com/exercicio/java/ex001/Main.java
+++ exercicio 001 - Hello World/src/br/com/exercicio/java/ex001/Main.java
@@ -4,7 +4,7 @@ package br.com.exercicio.java.ex001;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Faça um programa que exiba Hello Word na Tela");
+		System.out.println("Faça um programa que exiba Hello Word no console.");
 		
 		System.out.println("Hello World!");
 
